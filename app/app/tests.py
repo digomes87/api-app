f4b828a01bf8db310877067dfe0e9052891d2bbb
Diego Go <dgomes@Diegos-MacBook-Pro.local>
2022-05-29 16:35:14 -0300

    lint

--- app/app/tests.py
+++ app/app/tests.py
@@ -7,7 +7,7 @@ class CalcTests(SimpleTestCase):
         res = calc.add(5, 6)
 
         self.assertEqual(res, 11)
-    
+
     def test_subtract_numbers(self):
         res = calc.subtract(10, 15)
 
